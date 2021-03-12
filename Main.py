7856e1e56806ef0bad6970f377eb9a7855768e69
Scott Vanselow <profvanselow@gmail.com>
2021-03-11 19:07:29 -0500

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,10 @@
+def print_message(name_to_print):
+    print(name_to_print, "is really cool!")
+
+def main():
+    name = "Ryan"
+    print_message(name)
+    print_message("Jairo")
+    print_message("Kat")
+
+main()
